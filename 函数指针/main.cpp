866d764fd0828ba6e8e609b7cfffd2f55710edbd
plainchn <plainwe@gmail.com>
2021-10-27 20:11:33 +0800

    增加项目

--- 函数指针/main.cpp
+++ 函数指针/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-
 void print(int n, int m)
 {
-    cout << "n"
+    cout << "n";
 }
+
+// void foo(int i)
+// {
+//     cout << "this is normal function" << endl;
+// }
+void foo(int *i)
+{
+    cout << "this is ptr function" << endl;
+}
+
 int main()
 {
-
-
-
+    foo(NULL);
     cout << "hello world" << endl;
     return 0;
 }
