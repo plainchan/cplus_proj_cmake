525bf72c3ca5ce34e1b713d4cdd4e0c0126cd9f4
plainchan <plainwe@gmail.com>
2022-05-07 11:00:40 +0800

    共用体浮点准换

--- union_usage/main.cpp
+++ union_usage/main.cpp
@@ -17,14 +17,55 @@ union U_Demo
     int z;         // 4个字节
 };                 //共享内存
 
+/* 
+*   共用体用途：将float数据拆分成4字节数据，将4字节数据转换成float
+*/
+union U_Demo1
+{
+    float  f_data;
+    char   byte[4];
+};   
+union word
+{
+	short a;
+	char b;
+}c;
+ 
+int checkCPU(void)
+{
+	c.a = 1;
+	return (c.b == 1);
+}
 int main()
 {
-    U_Demo u;
-    
-    cout << "u的size = " << sizeof(u) << endl;
-    u.z = 0x41;                                 //65
-    cout << "u.x = " << u.x << endl;
-    cout << "u.y = " << u.y << endl;
-    cout << "u.z = " << u.z << endl;
+    {
+        U_Demo u;
+        
+        cout << "u的size = " << sizeof(u) << endl;
+        u.z = 0x41;                                 //65
+        cout << "u.x = " << u.x << endl;
+        cout << "u.y = " << u.y << endl;
+        cout << "u.z = " << u.z << endl;
+    }
+
+    {
+        //2.54   浮点数地址实际存放数据  0x40228f5c
+        U_Demo1 u;
+        u.byte[0]=0x5c;
+        u.byte[1]=0x8f;
+        u.byte[2]=0x22;
+        u.byte[3]=0x40;
+        
+        cout << "浮点数" << u.f_data << endl;
+    }
+    {
+
+        if ( checkCPU() )
+            cout << "Little_endian" << endl;
+        
+        else
+            cout << "Big_endian" << endl;
+    }
+
     return 0;
 }
