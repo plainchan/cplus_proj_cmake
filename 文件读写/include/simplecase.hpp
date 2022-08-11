42d14724a9707f71b0d8ce5ec73e8d2d7a767d51
plainchan <plainwe@gmail.com>
2022-08-11 23:05:22 +0800

    chang matplotlib cpp cmakelists

---
+++ 文件读写/include/simplecase.hpp
@@ -0,0 +1,35 @@
+#ifndef SIMPLE_CASE_HPP
+#define SIMPLE_CASE_HPP
+
+#include <fstream>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- 文件读写/src/main.cpp
+++ 文件读写/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
 
-    cout << "HelloWorld" << endl;
+    std::cerr << "HelloWorld" << endl;
     return 0;
 }
